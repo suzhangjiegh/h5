bf85c589649a5103ffa0dd5f102e1b23671524bd
suzhangjiegh <su2010yx@163.com>
2016-12-14 11:01:37 +0800

    修改启动方式

--- nodejs/app.js
+++ nodejs/app.js
@@ -7,7 +7,6 @@ var app = express();
 
 // 网页模板引擎
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 app.set( 'view engine', 'html' );
 app.engine( '.html', require( 'ejs' ).__express );
 
@@ -43,4 +42,65 @@ app.use(function(req, res, next){
 //路由配置
 require('./routes/index')(app);
 
-module.exports = app;
+//module.exports = app;
+var http = require('http');
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+var server = http.createServer(app);
+
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
+
+//端口号
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+//失败回调
+function onError(error) {
+  console.log('启动失败' +error);
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  var bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+  
+}
+
+//成功回调
+function onListening() {
+  var addr = server.address();
+  var bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
+}
